dea725d799b37a094a0ce47e0a0a248b512de0a3
george <egorleontev54@gmail.com>
2025-09-18 20:32:10 +0300

    feat: init seed

--- web-api/prisma/seed.ts
+++ web-api/prisma/seed.ts
@@ -16,10 +16,35 @@ async function main() {
     '0987654321',
     10,
   )})`;
+  await prisma.$executeRaw`insert into business.user (email, password) VALUES ('user2@example2.com', ${await bcrypt.hash(
+    '654321',
+    10,
+  )})`;
+  await prisma.$executeRaw`insert into business.user (email, password) VALUES ('user3@example3.com', ${await bcrypt.hash(
+    '123456',
+    10,
+  )})`;
+  await prisma.$executeRaw`insert into business.user (email, password) VALUES ('user4@gmail.com', ${await bcrypt.hash(
+    '09854321',
+    10,
+  )})`;
+  await prisma.$executeRaw`insert into business.user (email, password) VALUES ('user5@example2.com', ${await bcrypt.hash(
+    '09876543',
+    10,
+  )})`;
+  await prisma.$executeRaw`insert into business.user (email, password) VALUES ('user6@example3.com', ${await bcrypt.hash(
+    '34567890',
+    10,
+  )})`;
 
   await prisma.$executeRaw`truncate table business.player cascade`;
   await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('simple player', 1)`;
   await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('batman', 2)`;
+  await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('megaladon', 3)`;
+  await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('boss', 4)`;
+  await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('petya', 5)`;
+  await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('manboy', 6)`;
+  await prisma.$executeRaw`insert into business.player (user_name, user_id) VALUES ('vladimir', 7)`;
 }
 
 main()
